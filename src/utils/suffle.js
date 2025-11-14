a79c39970e753a86b1d17cd4355aebf0bf9f44ae
Vikas Solanki <solankivikas066@gmail.com>
2025-11-14 17:50:15 +0530

    feat: add shuffle utility for suffling categories

---
+++ src/utils/suffle.js
@@ -0,0 +1,9 @@
+export const suffleCategory = (category) => {
+  const result = [...category]
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]]
+  }
+  return result
+}
+
