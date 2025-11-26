f1592c5c1e3fe5e1ae129fc092c2f49ce3d93fa8
Vikas Solanki <solankivikas066@gmail.com>
2025-11-26 14:47:42 +0530

    feat(constants): add shared constants for game status and config

---
+++ src/shared/constants.js
@@ -0,0 +1,11 @@
+export const GAME_STATUS = {
+  IDLE: 'idle',
+  PLAYING: 'playing',
+  WON: 'won',
+  LOST: 'lost',
+  PAUSED: 'paused'
+}
+
+export const GAME_CONFIGS = {
+  MAX_WRONG_GUESSES: 4,
+}
